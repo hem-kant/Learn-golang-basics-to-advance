d759c7e96f49bdb8ff2b22db125ded3672873a34
hem-kant <hemkant.india@gmail.com>
2019-12-26 18:33:44 +0100

    interfaces reader and writer

--- Interfaces_HTTP/main.go
+++ Interfaces_HTTP/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://www.google.com")
@@ -14,4 +17,22 @@ func main() {
 	}
 
 	fmt.Println(resp)
+	fmt.Println("***** Reading HTML BODY by implementing Reader  ******")
+
+	bs := make([]byte, 9999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+	fmt.Println("***** COPY HTML BODY by implementing Write  ******")
+	io.Copy(os.Stdout, resp.Body)
+}
+
+// logWriter now implementing the Write Interface
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("MANY byte", len(bs))
+	return len(bs), nil
 }
