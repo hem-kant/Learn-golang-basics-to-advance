ddf2d0fa97483e1e550c60b1ac94e538225f7b55
hem-kant <hemkant.india@gmail.com>
2019-12-26 14:20:33 +0100

    Savefile and ReadFile from disk test cases

--- PlayingCards/deck_test.go
+++ PlayingCards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestnewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,25 @@ func TestnewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck length of 20, but got %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card  of Four of Clubs, but we got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in the deck, got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
